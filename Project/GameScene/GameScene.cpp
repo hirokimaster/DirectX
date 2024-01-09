161901844ff2535f381ab92bf3045ca9143803e2
ニヘイリュウダイ <115063428+hirokimaster@users.noreply.github.com>
2024-01-09 21:36:48 +0900

    save

--- Project/GameScene/GameScene.cpp
+++ Project/GameScene/GameScene.cpp
@@ -9,18 +9,18 @@ GameScene::~GameScene() {
 // 初期化
 void GameScene::Initialize() {
 
-	// 乱数生成器
-	/*randomEngine_ = particle_->random();
+	//乱数生成器
+	randomEngine_ = particle_->random();
 	
 	emit.count = 3;
 	emit.frequency = 0.5f;
 	emit.frequencyTime = 0.0f;
 	
 	texHandle_ = TextureManager::Load("resources/circle.png");
-	texHandle2_ = TextureManager::Load("resources/uvChecker.png");
+	/*texHandle2_ = TextureManager::Load("resources/uvChecker.png");
 	particle2_ = std::make_unique<ParticleSystem>();
 	particle2_->Initialize("cube.obj");
-	particle2_->SetTexHandle(texHandle2_);
+	particle2_->SetTexHandle(texHandle2_);*/
 	particle_ = std::make_unique<ParticleSystem>();
 	particle_->Initialize("plane.obj");
 	particle_->SetTexHandle(texHandle_);
@@ -28,19 +28,19 @@ void GameScene::Initialize() {
 	accelerationField.area.min = { -1.0f,-1.0f,-1.0f };
 	accelerationField.area.max = { 1.0f,1.0f,1.0f };
 	viewProjection_.Initialize();
-	*/
+	
 }
 
 // 更新
 void GameScene::Update() {
 
-	/*viewProjection_.UpdateMatrix();
+	viewProjection_.UpdateMatrix();
 
-	ImGui::Begin("camera");
+	/*ImGui::Begin("camera");
 	ImGui::DragFloat3("translate", &viewProjection_.translate.x, 0.01f, -100.0f, 100.0f);
 	ImGui::DragFloat3("rotate", &viewProjection_.rotate.x, 0.01f, -100.0f, 100.0f);
 	ImGui::End();
-	
+	*/
 	emit.frequencyTime += dt;
 	if (emit.frequency <= emit.frequencyTime) {
 		particles_.splice(particles_.end(), particle_->Emission(emit, randomEngine_));
@@ -61,32 +61,31 @@ void GameScene::Update() {
 		
 	}
 
-	for (std::list<Particle>::iterator particleItr2 = particles2_.begin();
-		particleItr2 != particles2_.end(); ++particleItr2) {
+	//for (std::list<Particle>::iterator particleItr2 = particles2_.begin();
+	//	particleItr2 != particles2_.end(); ++particleItr2) {
 
-		if (particle2_->IsCollision(accelerationField.area, (*particleItr2).worldTransform.translate)) {
-			(*particleItr2).velocity = Add((*particleItr2).velocity, Multiply(dt, accelerationField.acceleration));
-		}
+	//	if (particle2_->IsCollision(accelerationField.area, (*particleItr2).worldTransform.translate)) {
+	//		(*particleItr2).velocity = Add((*particleItr2).velocity, Multiply(dt, accelerationField.acceleration));
+	//	}
 
-		(*particleItr2).worldTransform.translate = Add((*particleItr2).worldTransform.translate, Multiply(dt, (*particleItr2).velocity));
-		(*particleItr2).worldTransform.UpdateMatrix();
+	//	(*particleItr2).worldTransform.translate = Add((*particleItr2).worldTransform.translate, Multiply(dt, (*particleItr2).velocity));
+	//	(*particleItr2).worldTransform.UpdateMatrix();
 
-	}
+	//}
 
-	
+	//
 
-	ImGui::Begin("Particle");
-	if (ImGui::Button("Add Particle")) {
-		particles_.splice(particles_.end(), particle_->Emission(emit, randomEngine_));
-	}
-	ImGui::End();*/
+	//ImGui::Begin("Particle");
+	//if (ImGui::Button("Add Particle")) {
+	//	particles_.splice(particles_.end(), particle_->Emission(emit, randomEngine_));
+	//}
+	//ImGui::End();
 	
 }
 
 // 描画
 void GameScene::Draw(){
 
-	//particle_->Draw(particles_, viewProjection_);
+	particle_->Draw(particles_, viewProjection_);
 	//particle2_->Draw(particles2_, viewProjection_);
-	//
 }
