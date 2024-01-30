f003cbb88d65e87b893f92415ae844fda895d4e2
ニヘイリュウダイ <115063428+hirokimaster@users.noreply.github.com>
2024-01-30 19:38:52 +0900

    曲線レールカメラ完成

--- Project/GameObject/RailCamera/RailCamera.cpp
+++ Project/GameObject/RailCamera/RailCamera.cpp
@@ -10,6 +10,20 @@ void RailCamera::Initialize(const Vector3& position, const Vector3& rotate) {
 	worldTransform_.translate = position;
 	worldTransform_.rotate = rotate;
 	worldTransform_.scale = { 1.0f, 1.0f, 1.0f };
+	// 曲線レールカメラ用の制御点
+	controlPoints_ = {
+
+		{0,0,2},
+		{0,0,4},
+		{0,0,8},
+		{0,5,16},
+		{0,0,32},
+		{0,5,64},
+		{0,10,96},
+		{-0,5,128},
+		{-0,0,0},
+
+	};
 
 	// ビュープロジェクションの初期化
 	camera_.Initialize();
@@ -20,7 +34,9 @@ void RailCamera::Initialize(const Vector3& position, const Vector3& rotate) {
 /// </summary>
 void RailCamera::Update() {
 
-	MoveOnRail(kDeltaTime);
+	UpdateEyePosition(kDeltaTime);
+	UpdateTargetPosition(kDeltaTime);
+	MoveOnRail();
 
 	// カメラの座標を画面表示する処理
 	ImGui::Begin("Camera");
@@ -30,25 +46,8 @@ void RailCamera::Update() {
 
 }
 
-Vector3 RailCamera::TargetPosition(float t) const
+void RailCamera::MoveOnRail()
 {
-	return CatmullRomPosition(controlPoints_,t + 0.3f);
-}
-
-Vector3 RailCamera::EyePosition(float t) const
-{
-	return CatmullRomPosition(controlPoints_, t);
-}
-
-void RailCamera::MoveOnRail(float dt)
-{
-	currentTime_ += dt;
-	
-	// 視点と注視点を更新
-	eye_ = EyePosition(currentTime_);
-	target_ = TargetPosition(currentTime_);
-
-	// 移動
 	worldTransform_.translate = eye_;
 
 	forward_ = Normalize(Subtract(target_, eye_));
@@ -58,19 +57,41 @@ void RailCamera::MoveOnRail(float dt)
 	worldTransform_.rotate.y = std::atan2(forward_.x, forward_.z);
 	worldTransform_.rotate.x = std::atan2(-forward_.y, velocityXZ);
 
-
-	// ワールドトランスフォームのワールド行列再計算
+	// 行列再計算
 	worldTransform_.matWorld = MakeAffineMatrix(
 		worldTransform_.scale, worldTransform_.rotate, worldTransform_.translate);
 
+	Matrix4x4 rotateX = MakeRotateXMatrix(worldTransform_.rotate.x);
+	Matrix4x4 rotateY = MakeRotateXMatrix(worldTransform_.rotate.y);
+	Matrix4x4 rotateZ = MakeRotateXMatrix(worldTransform_.rotate.z);
 
+	Matrix4x4 rotateMatrix = Multiply(Multiply(rotateX, rotateY), rotateZ);
+	Matrix4x4 scale = MakeScaleMatrix(worldTransform_.scale);
+	Matrix4x4 translate = MakeTranslateMatrix(worldTransform_.translate);
+
+	worldTransform_.matWorld = Multiply(scale, Multiply(rotateMatrix, translate));
+	
+	// ビュー行列を計算する
+	camera_.matView = Inverse(worldTransform_.matWorld);
+	worldTransform_.ParentUpdateMatrix();
+
+#ifdef _DEBUG
 	ImGui::Begin("視点");
 	ImGui::Text("eye_ = %f %f %f", eye_.x, eye_.y, eye_.z);
 	ImGui::Text("target_ = %f %f %f", target_.x, target_.y, target_.z);
 	ImGui::End();
-
-	// ビュー行列を計算する
-	camera_.matView = Inverse(worldTransform_.matWorld);
-	worldTransform_.ParentUpdateMatrix();
+#endif // _DEBUG
 	
+}
+
+void RailCamera::UpdateTargetPosition(float dt)
+{
+	currentTimeTarget_ += dt;
+	target_ = CatmullRomPosition(controlPoints_, currentTimeTarget_); // 注視点を更新
+}
+
+void RailCamera::UpdateEyePosition(float dt)
+{
+	currentTimeEye_ += dt;
+	eye_ = CatmullRomPosition(controlPoints_, currentTimeEye_); // 視点を更新
 }
--- Project/GameObject/RailCamera/RailCamera.h
+++ Project/GameObject/RailCamera/RailCamera.h
@@ -24,11 +24,23 @@ public:
 	/// </summary>
 	const WorldTransform& GetWorldTransform() { return worldTransform_; }
 
-	Vector3 TargetPosition(float t) const;
+	/// <summary>
+	/// レールカメラを動かす計算をする所
+	/// </summary>
+	void MoveOnRail();
 
-	Vector3 EyePosition(float t)const;
+	/// <summary>
+	/// 注視点の更新
+	/// </summary>
+	/// <param name="dt"></param>
+	void UpdateTargetPosition(float dt);
 
-	void MoveOnRail(float dt);
+	/// <summary>
+	/// 視点の更新
+	/// </summary>
+	/// <param name="dt"></param>
+	void UpdateEyePosition(float dt);
+
 
 private:
 	// ワールド変換データ
@@ -39,27 +51,13 @@ private:
 	Vector3 velocity_ = { 0.0f, 0.0f, 0.0f };
 	// 角度
 	Vector3 rotate_ = { 0.0f, 0.0f, 0.0f };
-	// 曲線レールカメラ用の点
-	std::vector<Vector3> controlPoints_ = {
-
-		{0,0,2},
-		{0,0,4},
-		{0,0,8},
-		{0,5,16},
-		{0,0,32},
-		{0,5,64},
-		{0,10,96},
-		{-0,5,128},
-		{-0,0,0},
-		
-	};
-
-
+	// 曲線レールカメラ用の制御点
+	std::vector<Vector3> controlPoints_{};
 	Vector3 eye_{};
 	Vector3 target_{};
 	Vector3 forward_{};
 	// カメラの進行時間
-	float currentTime_ = 0.0f;
-	float t_ = 0.0f;
+	float currentTimeEye_ = 0.0f; // 視点用
+	float currentTimeTarget_ = 0.2f;// 注視点用で経過時間を0.2早くしてずらす
 	const float kDeltaTime = 1.0f / 1200.0f;
 };
