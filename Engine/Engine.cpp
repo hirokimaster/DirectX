a9e42720108730fb018cafe29bd3f461f854f30e
ニヘイリュウダイ <115063428+hirokimaster@users.noreply.github.com>
2024-02-04 08:41:39 +0900

    タイトルバー名前にした

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -16,7 +16,7 @@ void Engine::Initialize(){
 
 
 	// ウィンドウの作成
-	win_->CreateGameWindow(L"DirectXGame");
+	win_->CreateGameWindow(L"LE2B_15_ニヘイ_リュウダイ");
 	// DirectX初期化
 	dxCommon_->Initialize(win_);
 	Input::Initialize();
